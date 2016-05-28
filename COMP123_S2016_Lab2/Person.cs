892ed51e081562ff4e3178390530dc3efe7f1bd0
gapillan <gpaillan@my.centennialcollege.ca>
2016-05-28 00:40:42 -0700

    Add comments

--- COMP123_S2016_Lab2/Person.cs
+++ COMP123_S2016_Lab2/Person.cs
@@ -6,7 +6,45 @@ using System.Threading.Tasks;
 
 namespace COMP123_S2016_Lab2
 {
+    /**
+     * This class defines a generic Person 
+     * 
+     *@class Person 
+     *@field _name {string} 
+     */
     public class Person
     {
+        // PRIVATE INSTANCE VARAIABLES 
+        private string _name; 
+
+        // PUBLIC PROPERTIES 
+
+        /**
+         * This is a property for our _name field 
+         * 
+         * @property {string} Name
+         */
+        public string Name 
+        {
+            get 
+            {
+                return this._name;
+            }
+
+            set 
+            {
+                this._name = value; 
+            }
+        }
+
+        /**
+         * This is the default empty constructor for the Person class 
+         * 
+         * @contructor Person
+         */
+        public Person() 
+        {
+
+        }
     }
 }
--- COMP123_S2016_Lab2/Program.cs
+++ COMP123_S2016_Lab2/Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace COMP123_S2016_Lab2
 {
+    /**
+     * This class is the "driver" class for our Program 
+     * 
+     * @class Program 
+     */ 
+
     public class Program
     {
+        /**
+         * The main method of our driver class Program 
+         * 
+         * @method Main 
+         * @param {string[]} args
+         */
         public static void Main(string[] args)
         {
+            // create a new instance of the Person class
+            Person person = new Person();
         }
     }
 }
